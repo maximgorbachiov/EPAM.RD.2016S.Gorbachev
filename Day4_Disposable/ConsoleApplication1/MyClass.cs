4ba2479b4ea3b1eb107577ba310a68710927f53b
maximgorbachiov <maksimka.gorbachev.95@gmail.com>
2016-07-20 16:24:46 +0300

    Day4 Disposable added

---
+++ Day4_Disposable/ConsoleApplication1/MyClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication1
+{
+    // TODO: The code below contains a lot of issues. Please, fix all of them.
+    // Use as a guidelines:
+    // https://msdn.microsoft.com/en-us/library/b1yfkh5e(v=vs.110).aspx
+    // https://msdn.microsoft.com/en-us/library/b1yfkh5e%28v=vs.100%29.aspx?f=255&MSPPError=-2147217396
+    // https://msdn.microsoft.com/en-us/library/fs2xkftw(v=vs.110).aspx
+    public class MyClass : IDisposable
+    {
+        private IntPtr buffer;       // unmanaged resource
+        private SafeHandle resource; // managed resource
+        private bool disposed;
+
+        public MyClass()
+        {
+            buffer = Helper.AllocateBuffer();
+            resource = Helper.GetResource();
+        }
+
+        ~MyClass()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            if (disposing)
+            {
+                // managed 
+                resource?.Close();
+            }
+            // unmanaged 
+            Helper.DeallocateBuffer(buffer);
+            disposed = true;
+        }
+
+        public void DoSomething()
+        {
+            // NOTE: Manupulation with _buffer and _resource in this line.
+            if (disposed)
+            {
+                throw new ObjectDisposedException(ToString());
+            }
+        }
+    }
+}
